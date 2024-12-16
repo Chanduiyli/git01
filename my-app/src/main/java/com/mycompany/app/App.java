47143e57dddc2cea5b7512a4e216550c63f6fc4b
chanduiyli <chandu.iyli@gmail.com>
2024-12-16 05:19:03 +0000

    changes updated

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -7,5 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello CHANDU & RAVEENA !");
         System.out.println("Welcome to the HYDERABAD ");
+	System.out.println("Welcome to GDK");
+
     }
 }
