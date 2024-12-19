51b84cafa595ea4b865b76091cd877196e78f527
chanduiyli <chandu.iyli@gmail.com>
2024-12-19 04:41:13 +0000

    Changes made world

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,10 @@
 package com.mycompany.app;
 
 /**
- * Hello world!###########
+ * Hello world###########
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello CHANDU & RAVEENA !");
-        System.out.println("Welcome to the HYDERABAD ");
-	System.out.println("Welcome to GDK");
-
+        System.out.println("Hello CHANDU WORLD !");
     }
 }
