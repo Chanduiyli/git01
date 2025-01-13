9fe6fdaf18b85ec0ebb3a6505c9e3a6fdf0c5bd9
Chanduiyli <chandu.iyli@gmail.com>
2025-01-13 17:25:20 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,7 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello  new_york");
+        System.out.println("Hello  new_york!!!");
         System.out.println("Hello chandra shekar!");
 	System.out.println("Hello iyli");
 	System.out.println(" New line added");
