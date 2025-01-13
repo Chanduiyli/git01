725dd40f5161d62b23e0c1633f6d2d2b21002720
root <root@ip-172-31-2-68.ap-south-1.compute.internal>
2025-01-13 10:59:41 +0000

    new line added

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello chandra shekar!");
 	System.out.println("Hello iyli");
+	System.out.println(" New line added");
 
     }
 }
