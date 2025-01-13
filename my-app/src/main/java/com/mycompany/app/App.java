7db2d7316798af440342c1a7dc3e9c690a7fd28d
root <root@ip-172-31-2-68.ap-south-1.compute.internal>
2025-01-13 11:12:21 +0000

    main

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,9 +6,7 @@ package com.mycompany.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-	System.out.println("Hello iyli");
-	System.out.println("Hello chandu");
+        System.out.println("Hello  newyork");
 
     }
 }
