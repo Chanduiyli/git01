c0e64a4bb5f90925571e2c42252bf893ce7d0d7e
root <root@ip-172-31-14-36.ap-south-1.compute.internal>
2025-02-28 15:59:13 +0000

    Country names

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -2,14 +2,13 @@ package com.mycompany.app;
 
 /**
  * Hello world!
- * 
+ *
  */
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Hello  new_york!!!");
-        System.out.println("Hello chandra shekar!");
-	System.out.println("Hello iyli");
+        System.out.println(" Hello India ");
 
 
     }
